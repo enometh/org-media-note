6768d43efc518d2f2c54581c2e92089d3f62373f
Madhu <enometh@net.meer>
2023-04-23 08:16:29 +0530

    org-media-note-org-ref.el: conditionalize code that requires org-contrib/org-eldoc
    * org-media-note-org-ref.el: (org-media-note-setup-org-ref): avoid
    advicng the function org-eldoc-documentation-function unless it is
    defined.  This function is not in org-mode but in the org-contrib
    repository.

--- org-media-note-org-ref.el
+++ org-media-note-org-ref.el
@@ -162,8 +162,10 @@
                              :help-echo #'org-media-note-help-echo))
 
   ;; Display media link description in minibuffer when cursor is over it.
-  (advice-add #'org-eldoc-documentation-function
-              :before-until #'org-media-note-display-media-cite-link-message-in-eldoc))
+  (when (fboundp 'org-eldoc-documentation-function)
+    (advice-add #'org-eldoc-documentation-function
+		:before-until #'org-media-note-display-media-cite-link-message-in-eldoc)))
+
 
 ;;;; Footer
 (provide 'org-media-note-org-ref)
